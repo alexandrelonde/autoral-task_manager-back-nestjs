9b4ec5fba7ab275675ade994a1d884ba96c1a1cf
Alexandre Londe <alexandrelonde.x@gmail.com>
2025-02-12 13:32:30 -0300

    feat: criando a tasks.controller.ts

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+// eslint-disable-next-line prettier/prettier
+import { Body, Controller, Post, Get, Param, Patch, Delete } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Post()
+  create(@Body('title') title: string) {
+    return this.tasksService.create(title);
+  }
+
+  @Get()
+  findAll() {
+    return this.tasksService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.tasksService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body('isCompleted') isCompleted: boolean) {
+    return this.tasksService.update(+id, isCompleted);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.tasksService.remove(+id);
+  }
+}
